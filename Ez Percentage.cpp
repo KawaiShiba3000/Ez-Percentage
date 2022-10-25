7e39dcc0b374fb67fcfc68103f144f0dae2436c5
KawaiShiba3000 <115523865+KawaiShiba3000@users.noreply.github.com>
2022-10-25 16:46:35 +0530

    Ez Updated

--- Ez Percentage.cpp
+++ Ez Percentage.cpp
@@ -1,44 +1,44 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    /*enter you marks of five subjects with 
-    space between each of them.....then give 
-    another space and enter total max. marks
-    of your exam....
-    this software will help you get your
-    total marks out of max. marks and will 
-    also give you the percentage you got*/
+/*enter you marks of five subjects with 
+space between each of them.....then give 
+another space and enter total max. marks
+of your exam....
+this software will help you get your
+total marks out of max. marks and will 
+also give you the percentage you got*/
 
 
-    int main() {
+int main() {
 
-    float a;
-    float b;
-    float c;
-    float d;
-    float e;
+float a;
+float b;
+float c;
+float d;
+float e;
     
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cin>>d;
-    cin>>e;
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;
+cin>>e;
 
-    cout << "Entered Marks : " << a << " " << b << " " << c << " " << d << " " << e << " " << endl;
-
-    float marks = a+b+c+d+e;
-    float total;
-    cin >> total;
+float marks = a+b+c+d+e;
+float total;
+cin >> total;
     
-    cout << "Total Max. Marks : " << total << endl;
-    
-    float percentage = (marks*100)/total;
+float percentage = (marks*100)/total;
 
-    cout << "Marks Obtained In " << total << " : " << marks << endl;
+cout << "Entered Marks : " << a << " , " << b << " , " << c << " , " << d << " , " << e << endl;
 
-    cout << "Percentage : " << percentage << "%";
+cout << "Total Max. Marks : " << total << endl;
 
-    return 0;
+cout << "Marks Obtained In " << total << " : " << marks << endl;
+
+cout << "Percentage : " << percentage << "%";
+
+return 0;
 
 
-    }
+}
